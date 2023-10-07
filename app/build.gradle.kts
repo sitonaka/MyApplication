d9ad334bb65eae8fe67747f81c956aa96a7fce77
sitonaka <csitonaka@gmail.com>
2023-10-07 17:43:33 +0900

    step 57.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,22 +55,22 @@ android {
 
 dependencies {
 
-    val composeVersion = "1.5.0"
-    implementation("androidx.core:core-ktx:1.10.1")
+    val composeVersion = "1.5.3"
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:1.5.0")
+    implementation("androidx.compose.material:material:1.5.3")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
-    val navVersion = "2.7.1"
+    val navVersion = "2.7.4"
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
