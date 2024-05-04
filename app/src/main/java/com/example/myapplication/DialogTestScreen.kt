0f76a1114402c21776577966c27c563c6f562ceb
sitonaka <csitonaka@gmail.com>
2024-05-05 08:48:39 +0900

    step 78.

--- app/src/main/java/com/example/myapplication/DialogTestScreen.kt
+++ app/src/main/java/com/example/myapplication/DialogTestScreen.kt
@@ -2,48 +2,48 @@ package com.example.myapplication
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun DialogTestScreen() {
-    val showDialog = remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Button(onClick = {
-            showDialog.value = true
-        }) {
-            Text(text = "BUTTON")
+    val counter = rememberSaveable { mutableStateOf(0) }
+    val scope = rememberCoroutineScope()
+    DialogContent { showDialog ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = counter.value.toString())
+            Button(onClick = {
+                scope.launch {
+                    val result = showDialog(DialogData.COUNT_UP)
+                    if (result) {
+                        counter.value++
+                    }
+                }
+            }) {
+                Text(text = DialogData.COUNT_UP.kick)
+            }
+            Button(onClick = {
+                scope.launch {
+                    val result = showDialog(DialogData.COUNT_DOWN)
+                    if (result) {
+                        counter.value--
+                    }
+                }
+            }) {
+                Text(text = DialogData.COUNT_DOWN.kick)
+            }
         }
-    }
-    if (showDialog.value) {
-        AlertDialog(
-            onDismissRequest = {},
-            confirmButton = {
-                Button(onClick = {
-                    showDialog.value = false
-                }) {
-                    Text(text = "OK")
-                }
-            },
-            dismissButton = {
-                Button(onClick = {
-                    showDialog.value = false
-                }) {
-                    Text(text = "CANCEL")
-                }
-            },
-            title = { Text(text = "TITLE") },
-            text = { Text(text = "TEXT") })
     }
 }
 
